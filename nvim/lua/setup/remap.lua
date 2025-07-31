3cfb4a2e74089d10ea8445b67ad7c3e8f2301df3
AZTR3K <ahsan.17april@gmail.com>
2025-07-31 11:00:37 +0500

    Better pane navigation in nvim

--- nvim/lua/setup/remap.lua
+++ nvim/lua/setup/remap.lua
@@ -4,10 +4,10 @@
 vim.g.mapleader = " "
 
 -- Better pane navigation
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
 
 -- Yank
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
